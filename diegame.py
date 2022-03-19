dcb71952df0aa5c93fbf49e20019f0123c1716d3
saurishchanda <101108347+saurishchanda@users.noreply.github.com>
2022-03-19 22:57:35 +0530

    Update diegame.py

--- diegame.py
+++ diegame.py
@@ -3,6 +3,17 @@ import random
 print("****Welcome to the game****")
 dice=["1","2","3","4","5","6"]
 
-print("The die shows:")
-roll=random.choices(dice,weights=[98,1,1,1,1,98])
-print(roll)
+print("Die 1 shows:")
+roll1=random.choices(dice,weights=[1,1,1,1,1,45])
+print(roll1)
+
+
+print("Die 2 shows:")
+roll2=random.choices(dice,weights=[45,1,1,1,1,1])
+print(roll2)
+
+print("Die 3 shows:")
+roll3=random.choices(dice,weights=[45,1,1,1,1,1])
+print(roll3)
+
+
