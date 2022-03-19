8e344f6066fee75f1447f8ae181fe766f14b487d
saurishchanda <101108347+saurishchanda@users.noreply.github.com>
2022-03-19 19:22:24 +0530

    Update diegame.py
    This code is intended to simulate the roll of a die similar to while playing snakes and ladders. Here I have used the random module in python to manipulate the the die so that the probability of occurence of 1 and 6 is 98/200 while that of the other four numbers are  1/200 each.

--- diegame.py
+++ diegame.py
@@ -4,5 +4,5 @@ print("****Welcome to the game****")
 dice=["1","2","3","4","5","6"]
 
 print("The die shows:")
-roll=random.choices(dice,weights=[99,1,1,1,1,99])
+roll=random.choices(dice,weights=[98,1,1,1,1,98])
 print(roll)
